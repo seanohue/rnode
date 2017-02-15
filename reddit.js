eb02a4c0fc5bca80fda61d89d5665ad4fb77855f
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-02-15 13:03:53 -0500

    Update code to use ES6 standards where applicable. Use html decoder and install node fetch for more straightforward API for fetching subreddit data.

--- reddit.js
+++ reddit.js
@@ -1,121 +1,93 @@
-var HOST = "www.reddit.com";
-var TARGET_SUB_REDDIT = "/r/relationships";
-var TOP_COUNT = 5;
-var CLEAR_TERM = '\u001B[2J\u001B[0;0f';
+const HOST = "www.reddit.com";
+const TARGET_SUB_REDDIT = "/r/news";
+const TOP_COUNT = 5;
+const CLEAR_TERM = '\u001B[2J\u001B[0;0f';
 
-var http = require("http");
-var unescape = require('unescape');
-var striptags = require('striptags');
+const fetch = require('node-fetch');
+const decoder = require('html-decoder');
 
+const RedditPost = require('./lib/RedditPost');
+const RedditComment = require('./lib/RedditComment');
 
-var rl = require("readline")
+const rl = require("readline")
   .createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-var divider = "\n\n================================\n\n";
+const divider = `
+===========================================
+`;
 
 function main() {
-  getSubRedditData(TARGET_SUB_REDDIT, (result) => {
-    startSubRedditGopher(result);
-  });
-}
-
-//***** Classes
-function RedditPost(post) {
-  console.log(post);
-  this.title = post.data.title;
-  this.body = post.data.selftext_html;
-  this.permalink = post.data.permalink;
-  this.author = post.data.author;
-  this.createdUtc = post.data.created_utc;
-  this.name = post.data.name;
-  this.commentsModel = null;
-
-  this.setCommentsModel = function(commentsModel) {
-    this.commentsModel = commentsModel;
-  };
-  this.getCommentsModel = function() {
-    return this.commentsModel;
-  };
-}
-
-function RedditCommentsModel(comment) {
-  this.source = comment;
-  this.title = comment[0].data.children[0].data.title;
-  this.author = comment[0].data.children[0].data.author;
-  this.comments = comment[1].data.children;
+  getSubRedditData(TARGET_SUB_REDDIT, startSubRedditGopher);
 }
 
 //***** Methods
 function getSubRedditData(path, success) {
 
-  var req = {
+  const req = {
     host: HOST,
     path: path + ".json"
   };
 
   return http.get(req, (response) => {
-    var body = "";
-
+    let body = "";
     response.on("data", (d) => {
+      console.log(response);
+      console.log(d);
       body += d;
     });
 
     response.on("end", () => {
       try {
-        var parsed = JSON.parse(body);
+        const parsed = JSON.parse(body);
         success(parsed);
       } catch (err) {
-        console.log("There was an error parsing the json response:\n" +
-          err);
+        console.log("There was an error parsing the json response:", err, body);
       }
     });
 
     response.on("error", (e) => {
-      console.log("there was an error " + e);
+      console.log("There was an error " + e);
     });
 
   });
 }
 
 function getArrayOfRedditPosts(postArray) {
-  var arr = [];
-  postArray.forEach((item) => {
-    arr.push(new RedditPost(item));
-  });
-  return arr;
+  return postArray.map(item => new RedditPost(item));posts = [];
 }
 
 function showRedditComments(item) {
   clearTerminal();
   writeHeader("comments");
-  var commentModel = item.getCommentsModel();
+  const commentModel = item.getCommentsModel();
 
   console.log(commentModel.title + "\n\n");
 
   if (commentModel.comments.length > 0) {
-    var len = (commentModel.comments.length < TOP_COUNT) ?
-      commentModel.comments.length : TOP_COUNT;
+    const len = (commentModel.comments.length < TOP_COUNT) ?
+      commentModel.comments.length :
+      TOP_COUNT;
 
-    for (var i = 1; i < len; i++) {
-      var comment = commentModel.comments[i].data;
-      console.log(" %s. [" + comment.author + "] - " + comment.body + '\n\n\n',
-        i);
+    for (let i = 1; i < len; i++) {
+      const comment = commentModel.comments[i].data;
+      console.log(` %s. [${comment.author}] - ${comment.body}
+
+
+      `);
     }
 
   } else {
     console.log("There are no comments.");
   }
 
-  rl.question("\nEnter to continue", function(key) {
-    main();
-  });
+  rl.question("\nEnter to continue", main);
 }
 
 function startSubRedditGopher(jsonResponse) {
-  var items = getArrayOfRedditPosts(jsonResponse.data.children);
+  const items = getArrayOfRedditPosts(jsonResponse.data.children);
   clearTerminal();
   writeHeader("topics");
   for (var i = 1; i <= TOP_COUNT; i++) {
@@ -126,7 +98,7 @@ function startSubRedditGopher(jsonResponse) {
     if (selection > 0 && selection <= TOP_COUNT) {
 
       getSubRedditData(items[selection].permalink, function(result) {
-        items[selection].setCommentsModel(new RedditCommentsModel(
+        items[selection].setCommentsModel(new RedditComment(
           result));
         showRedditComments(items[selection]);
       });
@@ -137,27 +109,29 @@ function startSubRedditGopher(jsonResponse) {
 }
 
 function writeHeader(arg) {
-  console.log("Welcome to the " + TARGET_SUB_REDDIT + " Gopher system.\n");
-  console.log("Here are top " + TOP_COUNT + " most recent %s:" + divider, arg);
+  console.log(`Welcome to the ${TARGET_SUB_REDDIT} Gopher system.`);
+  console.log(`Here are top ${TOP_COUNT} most recent : ${divider} ${arg}`);
 }
 
 function getItemRow(item) {
-  return item.title + "\n" +
-    "   by: " + item.author + " on " +
-    new Date(item.createdUtc * 1000).toString() + divider + cleanHtml(
-      item.body) + "\n";
+  return `${item.title}
+  by: ${item.author} on ${getFormattedDateAndPost(item)}
+`;
+}
+
+function getFormattedDateAndPost(item) {
+  return new Date(item.createdUtc * 1000).toString() + divider + cleanHtml(item.body)
 }
 
 function cleanHtml(content) {
-  return striptags(unescape(content))
+  return decoder.decode(content)
     .replace('-- SC_OFF --', '')
-    .replace('-- SC_ON --', '')
-    .replace('&#39;', '\'')
-    .replace('&quot;', '"');
+    .replace('-- SC_ON --', '');
 }
 
 function clearTerminal() {
   console.log(CLEAR_TERM);
 }
+
 //***** BEGIN
 main();
